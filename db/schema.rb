181d7ec6e972200e9e847dcaf19be8a2086526bf
FrozenCrash <zzcrash@gmail.com>
2017-10-23 23:20:25 +0300

    Add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023103718) do
+ActiveRecord::Schema.define(version: 20171023192918) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171023103718) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "text"
     t.index ["user_id"], name: "index_links_on_user_id"
   end
 
