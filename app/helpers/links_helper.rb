0c6535e194d74e69668f2fd6e62996694bfce09b
FrozenCrash <zzcrash@gmail.com>
2017-10-24 13:00:06 +0300

    Fixed description save and link_helper

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,11 @@
 module LinksHelper
+  def link_to_video_clip?(link)
+    link.url[0..4].include?("https") or link.url[0..3].include?("www.") 
+  end
+
+  def add_https_for_links(link)
+    if link.url.include?("www")
+      "https://" + link.url
+    end
+  end
 end
